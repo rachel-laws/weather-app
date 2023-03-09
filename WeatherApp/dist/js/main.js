06dc8ecc29bde591d994ed9bfe814cdddfb5cf0e
Rachel <123911257+rachel-laws@users.noreply.github.com>
2023-03-09 02:45:40 +0000

    Initial SCSS complete, created js file and folder

---
+++ WeatherApp/dist/js/main.js
@@ -0,0 +1,15 @@
+/* <div class="icon" id='icon'>
+          <i class='far fa-sun' aria-hidden='true' title='clear sky'></i>
+        </div>
+        <div class="temp">59°
+          <div class='unit'>F</div>
+        </div>
+        <div class="desc">Clear Sky</div>
+        <div class="feels">Feels like 53°</div>
+        <div class="maxtemp">High 62°</div>
+        <div class="mintemp">Low 39°</div>
+        <div class="humidity">Humidity 44%</div>
+        <div class="wind">Wind 7 mph
+    </div> */
+
+// 1:54:07 https://www.youtube.com/watch?v=s_Ie_yh_4Co
